65bbefd90a863f909a3fbe3ffcd3797b3c61e07c
Kirill Vasilev <kirillwork56@gmail.com>
2026-07-14 15:43:02 +0300

    Add DB

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
-
     static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
