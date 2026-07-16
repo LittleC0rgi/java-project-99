35e6194ce5d00a3b6619143301c094948a298194
Kirill Vasilev <kirillwork56@gmail.com>
2026-07-16 15:00:10 +0300

    Add initial user

---
+++ src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -0,0 +1,26 @@
+package hexlet.code.app.component;
+
+import hexlet.code.app.model.User;
+import hexlet.code.app.service.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+    @Autowired
+    private final CustomUserDetailsService userService;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        var email = "hexlet@example.com";
+        var userData = new User();
+        userData.setEmail(email);
+        userData.setPassword("qwerty");
+
+        userService.createUser(userData);
+    }
+}
