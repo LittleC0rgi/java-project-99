5d74e2fba8837b0f4a7820241e161e5639558c15
Kirill Vasilev <kirillwork56@gmail.com>
2026-07-16 17:14:57 +0300

    Add login and test for them

---
+++ src/main/java/hexlet/code/app/controller/AuthenticationController.java
@@ -0,0 +1,40 @@
+package hexlet.code.app.controller;
+
+import hexlet.code.app.dto.user.AuthRequest;
+import hexlet.code.app.utils.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthenticationController {
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> create(@RequestBody AuthRequest authRequest) {
+        try {
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    authRequest.getUsername(), authRequest.getPassword());
+
+            authenticationManager.authenticate(authentication);
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Неверный логин или пароль");
+        }
+
+        var token = jwtUtils.generateToken(authRequest.getUsername());
+        return ResponseEntity.ok(token);
+    }
+}
---
+++ src/test/java/hexlet/code/app/controller/AuthenticationControllerTest.java
@@ -0,0 +1,107 @@
+package hexlet.code.app.controller;
+
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AuthenticationControllerTest {
+    private static final String PATH = "/api/login";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        var user = new User();
+        user.setEmail(faker.internet().emailAddress());
+        user.setFirstName(faker.name().firstName());
+        user.setLastName(faker.name().lastName());
+        user.setPassword(passwordEncoder.encode("qwerty"));
+
+        var savedUser = userRepository.save(user);
+
+        var data = new HashMap<>();
+        data.put("password", "qwerty");
+        data.put("username", savedUser.getEmail());
+
+        var request = post(PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(org.hamcrest.Matchers.not(org.hamcrest.Matchers.emptyOrNullString())));
+    }
+
+    @Test
+    public void testLoginWithNonExistentUser() throws Exception {
+        var data = new HashMap<>();
+        data.put("username", faker.internet().emailAddress());
+        data.put("password", "any-password");
+
+        var request = post(PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testLoginWithWrongPassword() throws Exception {
+        var user = new User();
+        user.setEmail(faker.internet().emailAddress());
+        user.setFirstName(faker.name().firstName());
+        user.setLastName(faker.name().lastName());
+        user.setPassword(passwordEncoder.encode("qwerty"));
+
+        var savedUser = userRepository.save(user);
+
+        var data = new HashMap<>();
+        data.put("username", savedUser.getEmail());
+        data.put("password", "wrong-password");
+
+        var request = post(PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isUnauthorized());
+    }
+}
