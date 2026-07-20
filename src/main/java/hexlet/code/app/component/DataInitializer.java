1d9448fa8cc7bc48629383578418d9876bb19e9d
Kirill Vasilev <kirillwork56@gmail.com>
2026-07-20 11:55:21 +0300

    Update DataInitializer

--- src/main/java/hexlet/code/app/component/DataInitializer.java
+++ src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -1,6 +1,8 @@
 package hexlet.code.app.component;
 
+import hexlet.code.app.model.TaskStatus;
 import hexlet.code.app.model.User;
+import hexlet.code.app.repository.TaskStatusRepository;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @AllArgsConstructor
@@ -17,6 +21,9 @@ public class DataInitializer implements ApplicationRunner {
 
     @Autowired
     private final CustomUserDetailsService userService;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
 
     @Override
     public void run(ApplicationArguments args) {
@@ -28,5 +35,22 @@ public class DataInitializer implements ApplicationRunner {
         if (userRepository.findByEmail(email).isEmpty()) {
             userService.createUser(userData);
         }
+
+        var defaultStatuses = Map.of(
+                "draft", "Draft",
+                "to_review", "To review",
+                "to_be_fixed", "To be fixed",
+                "to_publish", "To publish",
+                "published", "Published"
+        );
+
+        defaultStatuses.forEach((slug, name) -> {
+            if (taskStatusRepository.findBySlug(slug).isEmpty()) {
+                var status = new TaskStatus();
+                status.setName(name);
+                status.setSlug(slug);
+                taskStatusRepository.save(status);
+            }
+        });
     }
 }
